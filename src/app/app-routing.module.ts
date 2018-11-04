00743f109b0dbbd259aeb483d506814aa90fdfee
nolte <karsten.nolte@itemis.de>
2018-11-04 15:21:18 +0100

    App an die Vue App angepasst

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './views/home/home.component';
+import {ComponentsComponent} from './views/components/components.component';
+import {AboutComponent} from './views/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'components', component: ComponentsComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {HomeComponent} from './views/home/home.component';
+import {ComponentsComponent} from './views/components/components.component';
+import {AboutComponent} from './views/about/about.component';
+import {CounterComponent} from './components/counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    AboutComponent,
+    HomeComponent,
+    CounterComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
